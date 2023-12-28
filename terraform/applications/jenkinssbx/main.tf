21a834e62e1f9dd613411af351f0c5b8bea0a2e0
Mike Russell <michael.russell@suse.com>
2023-12-27 23:36:11 -0800

    give up trying to pass in ssh option... try just setting it at global level

--- terraform/applications/jenkinssbx/main.tf
+++ terraform/applications/jenkinssbx/main.tf
@@ -86,7 +86,8 @@ resource "ansible_playbook" "jenkinssbxvm-vm-ansible-playbook" {
   playbook                = "ansible/jenkinssbx-vm.yml"
 
   # Inventory configuration
-  name   = "${var.JENKINSSBXVM_NAME} ansible_host=${harvester_virtualmachine.jenkinssbxvm-vm.network_interface[0].ip_address} ansible_sudo_pass=${var.JENKINSSBXVM_VM_PW} ansible_ssh_user=ubuntu ansible_ssh_password=${var.JENKINSSBXVM_VM_PW} ansible_ssh_common_args='-o StrictHostKeyChecking=no UserKnownHostsFile=/dev/null'"  # name of the host to use for inventory configuration
+  # remove  UserKnownHostsFile=/dev/null - just on box, running ansible... configure it at gloval /etc/ssh/ssh_config level... maybe...
+  name   = "${var.JENKINSSBXVM_NAME} ansible_host=${harvester_virtualmachine.jenkinssbxvm-vm.network_interface[0].ip_address} ansible_sudo_pass=${var.JENKINSSBXVM_VM_PW} ansible_ssh_user=ubuntu ansible_ssh_password=${var.JENKINSSBXVM_VM_PW} ansible_ssh_common_args='-o StrictHostKeyChecking=no'"  # name of the host to use for inventory configuration
 
   check_mode = false
   diff_mode  = false
