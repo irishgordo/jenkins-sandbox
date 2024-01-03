d3040a7403b4024bfade1faf71678af6132ca59b
Mike Russell <michael.russell@suse.com>
2024-01-03 12:26:56 -0800

    move docker to community plugin

--- terraform/applications/jenkinssbx/ansible/roles/docker-jenkins/files/build_docker_image.sh
+++ terraform/applications/jenkinssbx/ansible/roles/docker-jenkins/files/build_docker_image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+cd /home/ubuntu
 # generate the Ansible SSH keypair if it doesn't exist
 if [ ! -f './jenkins_ansible_key' ] ; then
     ssh-keygen -f jenkins_ansible_key -q -N ""
 fi
 #export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
 #sudo chown ubuntu:ubuntu $DOCKER_HOST
-/usr/bin/docker -H unix:///var/run/docker.sock build -t jenkins/jenkins-ansible:latest .
+#/usr/bin/docker -H unix:///var/run/docker.sock build -t jenkins/jenkins-ansible:latest .
