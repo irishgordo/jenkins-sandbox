ba95baf74701702865a20ccac952c52d1012699e
Mike Russell <michael.russell@suse.com>
2024-01-04 03:01:12 -0800

    fixing...

--- terraform/applications/jenkinssbx/ansible/roles/docker-jenkins/files/build_docker_image.sh
+++ terraform/applications/jenkinssbx/ansible/roles/docker-jenkins/files/build_docker_image.sh
@@ -6,4 +6,4 @@ if [ ! -f './jenkins_ansible_key' ] ; then
 fi
 #export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
 #sudo chown ubuntu:ubuntu $DOCKER_HOST
-#/usr/bin/docker -H unix:///var/run/docker.sock build -t jenkins/jenkins-ansible:latest .
+/usr/bin/docker build -t jenkins/jenkins-ansible:latest .
